2defad9127ab952143cc54f55f9bd3da1d87884b
Anastasiia <63855865+Avenz@users.noreply.github.com>
2020-05-01 00:35:25 +0800

    Update and rename pump to pump_config.h

---
+++ include/pump_config.h
@@ -0,0 +1,3 @@
+#define PUMP_ID		"859106"	// pump serial number (note that this is a string constant)
+#define PUMP_FREQUENCY	868500000	// pump frequency
+#define MMTUNE_START	868300000	// starting frequency for mmtune scans
