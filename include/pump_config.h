84114fb6440af2a26f19178e2faefd05a35ff3c0
Anastasiia <63855865+Avenz@users.noreply.github.com>
2020-05-01 00:37:41 +0800

    Delete pump_config.h

--- include/pump_config.h
+++
@@ -1,3 +0,0 @@
-#define PUMP_ID		"859106"	// pump serial number (note that this is a string constant)
-#define PUMP_FREQUENCY	868500000	// pump frequency
-#define MMTUNE_START	868300000	// starting frequency for mmtune scans
